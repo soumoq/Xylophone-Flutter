ddee6aad0ff5a40c6a06abfe738c828e5a650e42
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2021-08-13 01:35:38 +0530

    Complete Xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,38 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
-  void playAudio(int count){
-    AssetsAudioPlayer.newPlayer().open(Audio("assets/note$count.wav"));
+  void playAudio(int count) =>
+      AssetsAudioPlayer.newPlayer().open(Audio("assets/note$count.wav"));
+
+  Expanded buildKey(int count, MaterialColor buttonCuller) {
+    Expanded expanded = new Expanded(
+      child: MaterialButton(
+        color: buttonCuller,
+        onPressed: () {
+          playAudio(count);
+        },
+      ),
+    );
+    return expanded;
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                playAudio(1);
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
           ),
         ),
       ),
